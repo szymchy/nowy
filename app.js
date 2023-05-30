66dc918432f56cb62c4db851a6bf76a26cfec533
Michal <michal.zelechowski@o2.pl>
2023-05-30 20:32:43 +0200

    1000k gold plis1

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 console.log('jesteś ślizgaczem!');
 
+
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
 console.log('ale fajnie, jestę programerę!')
 
 
+
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
 console.log('Zostawcie pracownika1!!!1');
+
+
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
--- test.js
+++ test.js
@@ -1,7 +1,25 @@
 console.log('hej');
 
+
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
 console.log('fajnie jest');
 
+
+
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
+console.log('poka sowe')
+console.log("co sie stalo");
+
+console.log('poka sowe')
+console.log("co sie stalo");
 
 console.log('poka sowe')
 console.log("co sie stalo");
